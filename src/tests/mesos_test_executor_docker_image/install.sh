b7171f249317a3bfd096e86ace0f436eafa59161
Yifan Gu <guyifan1121@gmail.com>
2014-07-23 13:51:00 -0700

    Replaced the docker test executor with a smaller one.

--- src/tests/mesos_test_executor_docker_image/install.sh
+++ src/tests/mesos_test_executor_docker_image/install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-docker images | cut -d" " -f1 | grep -q mesos/test-executor
-if [ $? -ne 0 ]; then
-    docker build -t mesos/test-executor `dirname $0`
-fi
+echo "Skipping build, since we are using the test-executor from mesosphere"
+#docker images | cut -d" " -f1 | grep -q mesos/test-executor
+#if [ $? -ne 0 ]; then
+#    docker build -t mesos/test-executor `dirname $0`
+#fi
